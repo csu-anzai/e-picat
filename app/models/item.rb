3c133e8cf166fe260c77b72c3b93d6c14419523e
Nambinina <nambinina.solofonaina@gmail.com>
2019-09-03 15:01:13 +0300

    Ajout seed

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,6 @@ class Item < ApplicationRecord
 	has_many :item_orders
 	has_many :orders, through: :item_orders
 	validates :title, presence: true
-  validates :description,length: { in: 5..140 }
+  validates :description,length: { in: 5..300 }
   validates :price, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,19 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.first
+
+
+ 20.times do
+ 	name = Faker::Creature::Cat.name
+  description = "Le chat est un mammifère de la famille de félidés. Il pèse en moyenne 2,5 à 4,5 kg et mesure entre 66 et 76 cm, avec la queue. Ses pattes sont pourvues de griffes rétractiles"
+
+  description.length
+  item = Item.new
+  item.title = name
+  item.description = description
+  item.price = Faker::Number.between(from: 1.00, to: 10.00).round(2)
+  item.image_url = Faker::Avatar.image(slug: name, size: "200x200", format: "jpg", set: "set4", bgset: "bg1")
+
+  item.save
+ end
