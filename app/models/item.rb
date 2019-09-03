e7ba3bdc2a4140df710a197808e71ec92e5d7996
Nambinina <nambinina.solofonaina@gmail.com>
2019-09-03 13:24:52 +0300

    Validations Items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
 	has_many :item_orders
 	has_many :orders, through: :item_orders
+	validates :title, presence: true
+  validates :description,length: { in: 5..140 }
+  validates :price, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
 end
